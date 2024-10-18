400c7159f23359e62662c1066992609d78d9b2bc
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-10-19 04:25:50 +0600

    data showed on UI from DB

---
+++ src/components/Users.jsx
@@ -0,0 +1,21 @@
+
+import { useLoaderData } from 'react-router-dom';
+
+const Users = () => {
+
+    const users = useLoaderData()
+
+
+    return (
+        <div>
+            <h2>Users Number: {users.length}</h2>
+            <div>
+                {
+                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
