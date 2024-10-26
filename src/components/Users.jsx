63fdb7e266a3c8d8f35add748b8d5ad47079eec5
Moniruzzaman-Shawon <m.zaman.djp@gmail.com>
2024-10-27 01:24:20 +0600

    data deleted from UI and Server

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,15 +3,35 @@ import { useLoaderData } from 'react-router-dom';
 
 const Users = () => {
 
-    const users = useLoaderData()
+    const users = useLoaderData();
 
+    const handleDelete = _id => {
+        console.log(_id);
+        fetch(`http://localhost:4000/users/${_id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    alert('Deleted Successfully');
+                }
+
+            })
+
+
+    }
 
     return (
         <div>
             <h2>Users Number: {users.length}</h2>
             <div>
                 {
-                    users.map(user => <p key={user._id}>{user.name} : {user.email}</p>)
+                    users.map(user => <p
+                        key={user._id}>{user.name} : {user.email}
+                        <button
+                            onClick={() => handleDelete(user._id)}
+                        >X</button></p>)
                 }
             </div>
         </div>
